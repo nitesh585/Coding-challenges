ec5bd9796a07a5d48e728cf1141b946b3e6dc392
Niteshrocks <niteshyadav585@gmail.com>
2020-06-04 23:01:37 +0530

    Add file

---
+++ BFS/559.Maximum_Depth_of_N-ary_Tree.cpp
@@ -0,0 +1,61 @@
+Given a n-ary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Nary-Tree input serialization is represented in their level order traversal, each group of children is separated by the null value (See examples).
+
+ 
+
+Example 1:
+
+Input: root = [1,null,3,2,4,null,5,6]
+Output: 3
+
+Example 2:
+
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: 5
+
+ 
+
+Constraints:
+
+    The depth of the n-ary tree is less than or equal to 1000.
+    The total number of nodes is between [0, 10^4].
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    
+    int maxh = INT_MIN;
+    
+    int maxDepth(Node* root) {
+        if(!root) return 0;
+        
+        int tmp=0;
+        for(auto i  = root->children.begin() ; i!=root->children.end() ; i++){
+            tmp = max(maxDepth(*i),tmp);
+        }
+        
+        return max(maxh,tmp)+1;
+        
+    }
+};
