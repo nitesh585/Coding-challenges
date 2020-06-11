8fec5cc8d3bb95f58954702b02c3c0e98dc6248b
Niteshrocks <niteshyadav585@gmail.com>
2020-06-11 19:02:45 +0530

    Add files

---
+++ HASH TABLE/217.Contains_Duplicate.cpp
@@ -0,0 +1,34 @@
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> m;
+        for(auto i=nums.begin();i<nums.end();i++){
+            m[*i]++;
+            if(m[*i]==2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
